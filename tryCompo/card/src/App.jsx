afa6940e413f43851b9ad0a4f3c8557333e395e1
Mahesh <maheshddivate@gmail.com>
2024-11-20 18:09:04 +0530

    Adding My tried  Components

---
+++ tryCompo/card/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Card from './card/card';
+
+const App = () => {
+  return (
+    <div className="flex justify-center items-center min-h-screen bg-gray-100">
+      <Card
+       
+      />
+    </div>
+  );
+};
+
+export default App;
